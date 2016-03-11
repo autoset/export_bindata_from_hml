15f8d5d4d04144a8fd1e00110962b2a4086d015e
caoy/AutoSet Developer Group <caoy@autoset.org>
2016-03-11 22:32:53 +0900

    HWPML(HML)에서 참조하는 BinItem을 추출하여 실제 참조된 BinItem ID를 추려냅니다.

--- unique_reference_binitem.php
+++ unique_reference_binitem.php
@@ -4,7 +4,7 @@
  * 스크립트에 대한 저작권 고지
  * 
  * 이 스크립트는 한글과컴퓨터에서 만든 HWP의 XML 기반의 파일 포맷인
- * HWPML(HML)에서 참조하는 BinItem을 추출하는 스크립트입니다.
+ * HWPML(HML)에서 참조하는 BinItem을 추출하여 실제 참조된 BinItem ID를 추려냅니다.
  * 
  * 스크립트에 특별한 저작권을 주장하고 싶은 생각은 없으며,
  * HML을 이용하실 때 참고하시면 될 것 같습니다.
@@ -40,10 +40,14 @@ echo "Total: ".sizeof($arrBinItem).PHP_EOL;
 
 $idx = 0;
 
+$arrBinItemId = array();
+
 foreach ($arrBinItem as $binItem) {
-	echo "[".($idx++)."]";
-	echo $binItem->saveXML().PHP_EOL;
-	echo str_repeat("-", 75).PHP_EOL;
+	$arrBinItemId[] = $binItem['BinItem'];
 }
 
-echo "Done".PHP_EOL;
+$arrBinItemId = array_map('intval', $arrBinItemId);
+$arrBinItemId = array_unique($arrBinItemId, SORT_NUMERIC );
+sort($arrBinItemId);
+
+echo implode(PHP_EOL, $arrBinItemId);
